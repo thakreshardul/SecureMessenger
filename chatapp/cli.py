70416f4ff2f9093852f3d6d7d187a60476b92bcb
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-12-05 22:42:49 -0500

    More Fixes

--- chatapp/cli.py
+++ chatapp/cli.py
@@ -1,5 +1,4 @@
 import getpass
-import os
 import socket
 import sys
 
@@ -12,7 +11,6 @@ class TextInterface:
     def __init__(self, conf):
         self.client = client.ChatClient((conf.serverip, conf.serverport))
         client.udp.start(self.client, conf.clientip, conf.clientport, 1)
-
 
     def login(self):
         while True:
@@ -29,15 +27,23 @@ class TextInterface:
         print "Enter a command:\n1. list\n2. send <USER> <MESSAGE>\n3. quit\n"
         while True:
             command = raw_input()
-            userinput = command.split(" ")
+            userinput = command.split(" ", 2)
             if userinput[0] == "list":
                 l = self.client.list()
+
+                if l is None:
+                    print "List Failed"
+                    continue
+
                 if len(l) > 0:
                     print " ".join(l)
                 else:
                     print "Only you are logged in"
             elif userinput[0] == "send":
-                self.client.send(userinput[1], userinput[2])
+                if len(userinput) == 3:
+                    self.client.send(userinput[1], userinput[2])
+                else:
+                    print "Give user and message also"
             elif userinput[0] == "quit":
                 self.client.logout()
                 print ("Quitting the application")
